7d8c5fe66acc878a92af040372144f612f930910
Ibigelow92 <iobbigelow@gmail.com>
2023-02-18 16:11:25 -0800

    added basic functionality

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +18,27 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+## Description
+${data.description}
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Screenshot
+![alt-text](${data.screenshot})
+## Credits
+${data.credits}
+## Features
+${data.features}
+## How to contribute
+${data.contribution}
+##tests
+${data.tests}
+##creator
+${data.creator}
+##email
+${data.email}
 
 `;
 }
